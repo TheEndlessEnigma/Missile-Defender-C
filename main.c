95dd6a93931c444474943e118d2c2a8c1cbec68b
Chris Carpenter <52960628+TheEndlessEnigma@users.noreply.github.com>
2024-07-09 22:14:39 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 /*
+Missile Defender by Chris Carpenter
+
 BSD LICENSE
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
